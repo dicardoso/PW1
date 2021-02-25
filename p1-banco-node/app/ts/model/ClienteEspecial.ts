6fc7d561d3cc320ec8cf06858b93022c85d1443d
Diogo Cardoso <diogomescardoso@gmail.com>
2021-02-24 23:28:03 -0300

    Teste

--- p1-banco-node/app/ts/model/ClienteEspecial.ts
+++
@@ -1,6 +0,0 @@
-class ClienteEspecial extends Cliente {
-
-    constructor(_nome: string, _cpf: string, _conta: Conta ,  private _dependentes: Array<Cliente>){
-        super(_nome, _cpf, _conta);
-    }
-}
